73ae16656f41c7ec0f507c82ce61bf5c789fd53a
hansrajdeshpande18 <55242075+hansrajdeshpande18@users.noreply.github.com>
2021-10-06 21:11:06 +0100

    Add files via upload

---
+++ freecodeifstatement.py
@@ -0,0 +1,18 @@
+x = 5
+if x< 10:
+    print("smaller")
+if x > 20:
+    print("Bigger")
+print("Finish")
+
+#comparative operators
+# Boolen operator ask question and produce Yes or No result which we use to control program flow
+#Boolen expressions using comparison operators evaluate to True/False or Yes/NO
+#Comparison operators look at variables but do not change the variables
+#< Less than
+#<= less than or equal to
+
+# == equal to
+#>= greater than or equal to
+#> greater than
+#!= Not equal to
---
+++ test.py
@@ -0,0 +1,13 @@
+counter = 100
+miles = 1000.0
+name = "john"
+print(counter)
+print(miles)
+print(name)
+a = b = c = d = 10
+print(a)
+print(b)
+x, y, z = 1, 2, "JohnIbrahim"
+print(x)
+print(y)
+print(z)
